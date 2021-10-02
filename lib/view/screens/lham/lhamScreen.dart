5b59e01854d0b0a6550677cc4b1d30b9a99dc77a
lham <elhamsadeqi.es98@gmail.com>
2021-10-02 22:49:44 +0330

    bottom navigation bar first try

--- lib/view/screens/lham/lhamScreen.dart
+++ lib/view/screens/lham/lhamScreen.dart
@@ -6,6 +6,26 @@ class LhamScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/img/Profile.png")),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/img/Wallet.png")),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/img/Home.png")),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/img/Swap.png")),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/img/Time-Circle.png")),
+              title: Text("")),
+        ],
+      ),
       appBar: AppBar(
         title: Text("Lham"),
       ),
