876a29644c28035e7ea111591c70655d173edbee
asal <asalshojaee433@gmail.com>
2021-10-14 15:42:33 +0330

    add addTransactionScreen and update CardaddTransactionScreenWidget

--- lib/view/screens/asal/asalScreen.dart
+++ lib/view/screens/asal/asalScreen.dart
@@ -1,7 +1,11 @@
 import 'package:blubank/constant/colors.dart';
+import 'package:blubank/main.dart';
+import 'package:blubank/view/screens/asal/addTransactionScreen1.dart';
 import 'package:blubank/view/widgets/textInput_widget.dart';
 import 'package:blubank/view/widgets/button_widget.dart';
 import 'package:blubank/view/widgets/card_addTransactionScreen_widget.dart';
+import 'package:blubank/view/screens/mainScreen.dart';
+import 'package:blubank/view/widgets/cardTest_widget.dart';
 import 'package:flutter/material.dart';
 
 class AsalScreen extends StatelessWidget {
@@ -54,6 +58,7 @@ class AsalScreen extends StatelessWidget {
             bankImage: 'assets/img/bankimg4.png',
           ),
           TextInputWidget(),
+          CardTestWidget(title: "addTransactionScreen", route: AddTransactionScreen(),)
         ],
       ),
     );
