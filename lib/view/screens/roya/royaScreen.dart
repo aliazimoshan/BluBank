dec4edf86eedc6bdb76e66115ba69eaa0b39a0b0
roya <royam242@gmail.com>
2021-10-17 20:42:32 +0330

    modifed cardSwitch widget, finish Home Screen

--- lib/view/screens/roya/royaScreen.dart
+++ lib/view/screens/roya/royaScreen.dart
@@ -1,13 +1,43 @@
+import 'package:blubank/view/screens/HomeScreen.dart';
+import 'package:blubank/view/widgets/cardSwitchScreen.dart';
 import 'package:flutter/material.dart';
 
-class RoyaScreen extends StatelessWidget {
-  const RoyaScreen({Key? key}) : super(key: key);
+// import 'HomeScreen.dart';
 
+class RoyaScreen extends StatefulWidget {
+  @override
+  _RoyaScreenState createState() => _RoyaScreenState();
+}
+
+class _RoyaScreenState extends State<RoyaScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Roya"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: ElevatedButton(
+              child: Text('BluHomeScreen'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BluHomeScreen(),
+                  ),
+                );
+              },
+            ),
+          ),
+          CardSwitchScreen(
+            title: 'خدمات',
+            greyicon: Icons.checklist_rounded,
+            subtitle: 'بهترین خدمات ممکن',
+            iconColor: Colors.white,
+          ),
+        ],
       ),
     );
   }
